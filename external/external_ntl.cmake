0cc903ba2c07a72ac2e9e6c2f731b9599a9d8da0
Fabian Boemer <fabian.boemer@intel.com>
2018-04-12 09:29:36 -0700

    Set NTL include dir to ngraph_he_include_dir

--- external/external_ntl.cmake
+++ external/external_ntl.cmake
@@ -7,7 +7,7 @@ set(NTL_INCLUDE_DIR ${NTL_PREFIX}/include)
 set(NTL_LIB_DIR ${NTL_PREFIX}/lib )
 set(NTL_LIB_DIR ${NTL_PREFIX}/lib PARENT_SCOPE)
 
-message("Installing ntl at GMP_PREFIX: ${GMP_PREFIX}")
+message("Installing ntl at GMP_PREFIX: ${NGRAPH_HE_INSTALL_INCLUDE_DIR}")
 message("NTL_PREFIX ${NTL_PREFIX}")
 message("NTL_SOURCE_DIR  ${NTL_SOURCE_DIR}")
 
@@ -18,7 +18,7 @@ ExternalProject_Add(
     COMMAND tar -xzf ntl-10.5.0.tar.gz -C ${NTL_SOURCE_DIR} --strip 1
     COMMAND rm ntl-10.5.0.tar.gz
     PREFIX ${NTL_PREFIX}
-    CONFIGURE_COMMAND cd ${NTL_SOURCE_DIR}/src && ./configure NTL_GMP_LIP=on SHARED=on PREFIX=${NGRAPH_HE_INSTALL_DIR} GMP_PREFIX=${GMP_SOURCE_DIR}
+    CONFIGURE_COMMAND cd ${NTL_SOURCE_DIR}/src && ./configure NTL_GMP_LIP=on SHARED=on PREFIX=${NGRAPH_HE_INSTALL_DIR} GMP_PREFIX=${NGRAPH_HE_INSTALL_INCLUDE_DIR}
     BUILD_COMMAND make -j$(nproc) -C ${NTL_SOURCE_DIR}/src
     INSTALL_COMMAND make install -C ${NTL_SOURCE_DIR}/src && cp ${NTL_LIB_DIR}/libntl.so ${NGRAPH_INSTALL_LIB_DIR}
 )
