720a1ddd8c949e1b534ea1a716d177d33f2a995c
fboemer <fabiboemer@gmail.com>
2018-04-19 13:53:34 -0700

    Added HEPlainTensorView with encode/decode support for integer + float + double (#10)
    * Added SEALContext constructor for he_backend
    
    * Changed member variables to m_. Added SEAL keys, generator, encryptor/decryptor, int_evaluator to backend
    
    * Added he_plain_tensor_view
    
    * Modified constructors to take shared pointers
    
    * Read/write work on integer types
    
    * Switched to backend encode/decode function
    
    * Added int types to HEPlainTensorView
    
    * Added float/double encoding
    
    * Added cipher tensor view
    
    * Code format

--- src/he_backend.cpp
+++ src/he_backend.cpp
@@ -19,7 +19,36 @@
 using namespace ngraph;
 using namespace std;
 
-runtime::he::HEBackend::HEBackend()
+runtime::he::HEBackend::
+    HEBackend() // TODO: call HEBackend::HEBackend(seal::SEALContext& context) with default parameters
+{
+    seal::EncryptionParameters parms;
+    parms.set_poly_modulus("1x^2048 + 1");
+    parms.set_coeff_modulus(seal::coeff_modulus_128(2048));
+    parms.set_plain_modulus(1 << 8);
+    m_context = make_shared<seal::SEALContext>(parms);
+    m_int_encoder = make_shared<seal::IntegerEncoder>(m_context->plain_modulus());
+    m_frac_encoder = make_shared<seal::FractionalEncoder>(
+        m_context->plain_modulus(), m_context->poly_modulus(), 64, 32, 3);
+    m_keygen = make_shared<seal::KeyGenerator>(*m_context);
+    m_public_key = make_shared<seal::PublicKey>(m_keygen->public_key());
+    m_secret_key = make_shared<seal::SecretKey>(m_keygen->secret_key());
+    m_encryptor = make_shared<seal::Encryptor>(*m_context, *m_public_key);
+    m_decryptor = make_shared<seal::Decryptor>(*m_context, *m_secret_key);
+    m_evaluator = make_shared<seal::Evaluator>(*m_context);
+}
+
+runtime::he::HEBackend::HEBackend(seal::SEALContext& context)
+    : m_context(make_shared<seal::SEALContext>(context))
+    , m_int_encoder(make_shared<seal::IntegerEncoder>(m_context->plain_modulus()))
+    , m_frac_encoder(make_shared<seal::FractionalEncoder>(
+          m_context->plain_modulus(), m_context->poly_modulus(), 64, 32, 3))
+    , m_keygen(make_shared<seal::KeyGenerator>(*m_context))
+    , m_public_key(make_shared<seal::PublicKey>(m_keygen->public_key()))
+    , m_secret_key(make_shared<seal::SecretKey>(m_keygen->secret_key()))
+    , m_encryptor(make_shared<seal::Encryptor>(*m_context, *m_public_key))
+    , m_decryptor(make_shared<seal::Decryptor>(*m_context, *m_secret_key))
+    , m_evaluator(make_shared<seal::Evaluator>(*m_context))
 {
 }
 
@@ -30,7 +59,9 @@ runtime::he::HEBackend::~HEBackend()
 shared_ptr<runtime::TensorView>
     runtime::he::HEBackend::create_tensor(const element::Type& element_type, const Shape& shape)
 {
-    throw ngraph_error("Unimplemented");
+    throw ngraph_error("Not implemented");
+    //auto rc = make_shared<runtime::he::HECipherTensorView>(element_type, shape, "external");
+    //return static_pointer_cast<runtime::TensorView>(rc);
 }
 
 shared_ptr<runtime::TensorView> runtime::he::HEBackend::create_tensor(
@@ -54,4 +85,92 @@ bool runtime::he::HEBackend::call(std::shared_ptr<Function> func,
 void runtime::he::HEBackend::remove_compiled_function(std::shared_ptr<Function> func)
 {
     throw ngraph_error("Unimplemented");
+}
+
+void runtime::he::HEBackend::encode(seal::Plaintext* output,
+                                    const void* input,
+                                    const ngraph::element::Type& type)
+{
+    const std::string type_name = type.c_type_string();
+
+    if (type_name == "int32_t")
+    {
+        *output = m_int_encoder->encode(*(int32_t*)input);
+    }
+    else if (type_name == "int64_t")
+    {
+        *output = m_int_encoder->encode(*(int64_t*)input);
+    }
+    else if (type_name == "uint32_t")
+    {
+        *output = m_int_encoder->encode(*(uint32_t*)input);
+    }
+    else if (type_name == "uint64_t")
+    {
+        *output = m_int_encoder->encode(*(uint64_t*)input);
+    }
+    else if (type_name == "float")
+    {
+        *output = m_frac_encoder->encode(*(float*)input);
+    }
+    else if (type_name == "double")
+    {
+        *output = m_frac_encoder->encode(*(double*)input);
+    }
+    else
+    {
+        throw ngraph_error("Type not supported");
+    }
+}
+
+void runtime::he::HEBackend::decode(void* output,
+                                    const seal::Plaintext& input,
+                                    const ngraph::element::Type& type)
+{
+    const std::string type_name = type.c_type_string();
+
+    if (type_name == "int32_t")
+    {
+        int32_t x = m_int_encoder->decode_int32(input);
+        memcpy(output, &x, type.size());
+    }
+    else if (type_name == "int64_t")
+    {
+        int64_t x = m_int_encoder->decode_int64(input);
+        memcpy(output, &x, type.size());
+    }
+    else if (type_name == "uint32_t")
+    {
+        uint32_t x = m_int_encoder->decode_int64(input);
+        memcpy(output, &x, type.size());
+    }
+    else if (type_name == "uint64_t")
+    {
+        uint64_t x = m_int_encoder->decode_int64(input);
+        memcpy(output, &x, type.size());
+    }
+    else if (type_name == "float")
+    {
+        float x = m_frac_encoder->decode(input);
+        memcpy(output, &x, type.size());
+    }
+    else if (type_name == "double")
+    {
+        double x = m_frac_encoder->decode(input);
+        memcpy(output, &x, type.size());
+    }
+    else
+    {
+        throw ngraph_error("Type not supported");
+    }
+}
+
+void runtime::he::HEBackend::encrypt(seal::Ciphertext& output, seal::Plaintext& input)
+{
+    m_encryptor->encrypt(input, output);
+}
+
+void runtime::he::HEBackend::decrypt(seal::Plaintext& output, seal::Ciphertext& input)
+{
+    m_decryptor->decrypt(input, output);
 }
--- src/he_backend.hpp
+++ src/he_backend.hpp
@@ -33,18 +33,23 @@ namespace ngraph
         {
             class ExternalFunction;
             class HECallFrame;
+            class HETensorView;
+            class HEPlainTensorView;
+            class HECipherTensorView;
 
             class HEBackend : public runtime::Backend
             {
             public:
                 HEBackend();
+                HEBackend(seal::SEALContext& context);
+                HEBackend(HEBackend& he_backend) = default;
                 ~HEBackend();
 
                 std::shared_ptr<runtime::TensorView>
                     create_tensor(const element::Type& element_type, const Shape& shape) override;
 
                 std::shared_ptr<ngraph::runtime::TensorView>
-                    create_tensor(const ngraph::element::Type& elment_type,
+                    create_tensor(const ngraph::element::Type& element_type,
                                   const Shape& shape,
                                   void* memory_pointer) override;
 
@@ -56,8 +61,33 @@ namespace ngraph
 
                 void remove_compiled_function(std::shared_ptr<Function> func) override;
 
+                //void encrypt(const HECipherTensorView& output, const HEPlainTensorView& input);
+                //void decrypt(const HEPlainTensorView& output, const HECipherTensorView& input);
+
+                //void encode(const HEPlainTensorView& output, const TensorView& input);
+                //void decode(const TensorView& output, const HEPlainTensorView& input);
+
+                void encode(seal::Plaintext* output,
+                            const void* input,
+                            const ngraph::element::Type& type);
+                void decode(void* output,
+                            const seal::Plaintext& input,
+                            const ngraph::element::Type& type);
+
+                void encrypt(seal::Ciphertext& output, seal::Plaintext& input);
+                void decrypt(seal::Plaintext& output, seal::Ciphertext& input);
+
             private:
-                seal::EncryptionParameters params;
+                seal::EncryptionParameters parms;
+                std::shared_ptr<seal::SEALContext> m_context;
+                std::shared_ptr<seal::IntegerEncoder> m_int_encoder;
+                std::shared_ptr<seal::FractionalEncoder> m_frac_encoder;
+                std::shared_ptr<seal::KeyGenerator> m_keygen;
+                std::shared_ptr<seal::PublicKey> m_public_key;
+                std::shared_ptr<seal::SecretKey> m_secret_key;
+                std::shared_ptr<seal::Encryptor> m_encryptor;
+                std::shared_ptr<seal::Decryptor> m_decryptor;
+                std::shared_ptr<seal::Evaluator> m_evaluator;
             };
         }
     }
