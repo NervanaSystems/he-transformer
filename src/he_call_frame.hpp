9074ba367257b205f7e0bf72e504ce6e08603add
Yixing Lao <yixing.lao@intel.com>
2018-04-22 18:37:58 -0700

    clean up comments

--- src/he_call_frame.hpp
+++ src/he_call_frame.hpp
@@ -50,16 +50,12 @@ namespace ngraph
             public:
                 HECallFrame(const std::shared_ptr<Function>& function);
 
-                /// @brief Invoke the function with values matching the signature of the function.
-                ///
-                /// Tuples will be expanded into their tensor views to build the call frame.
                 void call(const std::vector<std::shared_ptr<runtime::TensorView>>& outputs,
                           const std::vector<std::shared_ptr<runtime::TensorView>>& inputs);
 
             private:
                 std::shared_ptr<Function> m_function;
                 HEBackend* he_backend;
-
                 void call(std::shared_ptr<Function> function,
                           const std::vector<std::shared_ptr<runtime::he::HETensorView>>& output_tvs,
                           const std::vector<std::shared_ptr<runtime::he::HETensorView>>& input_tvs);
