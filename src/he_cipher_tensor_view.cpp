af8b5b788424e833f2c00603b4a832e01659a558
Yixing Lao <yixing.lao@intel.com>
2018-04-22 18:30:21 -0700

    read done

--- src/he_cipher_tensor_view.cpp
+++ src/he_cipher_tensor_view.cpp
@@ -72,12 +72,12 @@ void runtime::he::HECipherTensorView::write(const void* source, size_t tensor_of
         throw out_of_range("Write access past end of tensor");
     }
 
-    size_t start_index = tensor_offset / type_byte_size; // inclusive
+    size_t dst_start_index = tensor_offset / type_byte_size;
     size_t num_elements_to_write = n / type_byte_size;
     for (size_t i = 0; i < num_elements_to_write; ++i)
     {
         const void* src_with_offset = (void*)((char*)source + i * type.size());
-        size_t dst_index = start_index + i;
+        size_t dst_index = dst_start_index + i;
         seal::Plaintext p;
         m_he_backend->encode(p, src_with_offset, type);
         m_he_backend->encrypt(*(m_cipher_texts[dst_index]), p);
@@ -86,22 +86,31 @@ void runtime::he::HECipherTensorView::write(const void* source, size_t tensor_of
 
 void runtime::he::HECipherTensorView::read(void* target, size_t tensor_offset, size_t n) const
 {
-    // const element::Type& type = get_element_type();
-    // if (tensor_offset / sizeof(seal::Ciphertext) + n / type.size() > m_num_elements)
-    // {
-    //     throw out_of_range("read access past end of tensor");
-    // }
+    const element::Type& type = get_element_type();
+    size_t type_byte_size = type.size();
 
-    // const vector<shared_ptr<seal::Ciphertext>>& source = m_cipher_texts;
+    // Memory must be byte-aligned to type_byte_size
+    // tensor_offset and n are all in bytes
+    if (tensor_offset % type_byte_size != 0 || n % type_byte_size != 0)
+    {
+        throw ngraph_error("tensor_offset and n must be divisable by type_byte_size.");
+    }
+    // Check out-of-range
+    if ((tensor_offset + n) / type_byte_size > m_num_elements)
+    {
+        throw out_of_range("Write access past end of tensor");
+    }
 
-    // size_t offset = tensor_offset / sizeof(seal::Ciphertext);
-    // for (int i = 0; i < n / type.size(); ++i)
-    // {
-    //     const shared_ptr<seal::Ciphertext> c = source[offset + i];
-    //     seal::Plaintext p;
-    //     m_he_backend->decrypt(p, *c);
-    //     m_he_backend->decode((void*)((char*)target + i * type.size()), p, type);
-    // }
+    size_t src_start_index = tensor_offset / type_byte_size;
+    size_t num_elements_to_read = n / type_byte_size;
+    for (size_t i = 0; i < num_elements_to_read; ++i)
+    {
+        void* dst_with_offset = (void*)((char*)target + i * type.size());
+        size_t src_index = src_start_index + i;
+        seal::Plaintext p;
+        m_he_backend->decrypt(p, *(m_cipher_texts[src_index]));
+        m_he_backend->decode(dst_with_offset, p, type);
+    }
 }
 
 size_t runtime::he::HECipherTensorView::get_size() const
