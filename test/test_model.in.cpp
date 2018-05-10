a12b56d083c9b75993c6cbfd84f86a221b234c52
Fabian Boemer <fabian.boemer@intel.com>
2018-05-10 10:40:18 -0700

    Started model loader

--- test/test_model.in.cpp
+++ test/test_model.in.cpp
@@ -17,6 +17,8 @@
 #include <ngraph/pass/manager.hpp>
 #include <ngraph/pass/visualize_tree.hpp>
 #include "ngraph/file_util.hpp"
+#include "loader.hpp"
+#include <unordered_map>
 
 TEST_F(TestHEBackend, tf_ptb)
 {
@@ -25,6 +27,21 @@ TEST_F(TestHEBackend, tf_ptb)
     const string json_path = file_util::path_join(HE_SERIALIZED_ZOO, model_name + ".js");
     const string json_string = file_util::read_file_to_string(json_path);
     shared_ptr<Function> f = deserialize(json_string);
+
+    // Get parameters
+    //string U = file_util::read_file_to_string("U.save");
+    unordered_map<string, vector<float>> parms;
+
+    parms["U"] = read_constant(file_util::path_join(HE_SERIALIZED_ZOO, "U.save"));
+    parms["W"] = read_constant(file_util::path_join(HE_SERIALIZED_ZOO, "W.save"));
+    parms["b"] = read_constant(file_util::path_join(HE_SERIALIZED_ZOO, "b.save"));
+    parms["Wy"] = read_constant(file_util::path_join(HE_SERIALIZED_ZOO, "Wy.save"));
+    parms["by"] = read_constant(file_util::path_join(HE_SERIALIZED_ZOO, "by.save"));
+    parms["x"] = read_constant(file_util::path_join(HE_SERIALIZED_ZOO, "x.save"));
+
+    const size_t num_hidden = 500;
+    const size_t time_steps = 10;
+    const size_t vocab_size = 50;
 
     // Visualize model
     auto model_file_name = model_name + string(".") + pass::VisualizeTree::get_file_ext();
@@ -40,10 +57,51 @@ TEST_F(TestHEBackend, tf_ptb)
     {
         auto& shape = parameter->get_shape();
         auto& type = parameter->get_element_type();
-        auto parameter_tv = backend->create_tensor(type, shape);
+        auto parameter_cipher_tv = backend->create_tensor(type, shape);
+		auto parameter_plain_tv = backend->create_plain_tensor(type, shape);
+
+		bool data_input = false;
+
+        if (shape == Shape(hidden_size, hidden_size))
+		{
+			copy_data(parameter_plain_tv, parms["U"]);
+		}
+		else if (shape == Shape(vocab_size, hidden_size))
+		{
+			copy_data(parameter_plain_tv, parms["W"]);
+		}
+		else if (shape == Shape(hidden_size, vocab_size))
+		{
+			copy_data(parameter_plain_tv, parms["Wy"]);
+		}
+		else if (shape == Shape(vocab_size))
+		{
+			copy_data(parameter_plain_tv, parms["by"]);
+		}
+		else if (shape == Shape(hidden_size))
+		{
+			copy_data(parameter_plain_tv, parms["b"]);
+		}
+		else if (shape == Shape(vocab_size, time_steps))
+		{
+			copy_data(parameter_cipher_tv, parms["x"]);
+			data_input = true;
+		}
+		else
+		{
+			throw ngraph_error("Unknown shape");
+		}
+
         NGRAPH_INFO << "created tensor of " << shape_size(shape) << " elements";
-        copy_data(parameter_tv, vector<float>(shape_size(shape)));
-        parameter_tvs.push_back(parameter_tv);
+
+if (data_input)
+{
+
+}
+else {
+	parameter_tvs.push_back(parameter_plain_tv);
+}
+        // copy_data(parameter_tv, vector<float>(shape_size(shape)));
     }
 
     auto results = f->get_results();
@@ -58,7 +116,7 @@ TEST_F(TestHEBackend, tf_ptb)
     NGRAPH_INFO << "calling function ";
     backend->call(f, result_tvs, parameter_tvs);
 
-    bool verbose = true;
+    bool verbose = false;
     if (verbose)
     {
         cout << "Result" << endl;
