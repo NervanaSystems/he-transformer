617e58fecba89cc5554b63405974783a8a8a3cd2
Fabian Boemer <fabian.boemer@intel.com>
2018-05-17 09:40:31 -0700

    Added heaan test

--- test/test_heaan.cpp
+++ test/test_heaan.cpp
@@ -14,16 +14,71 @@
 * limitations under the License.
 *******************************************************************************/
 
+#include <assert.h>
 #include <memory>
 
 #include "gtest/gtest.h"
 #include "heaan/heaan.hpp"
+#include "test_util.hpp"
 
 using namespace std;
+using namespace ngraph;
 
 TEST(heaan_example, trivial)
 {
     int a = 1;
     int b = 2;
     EXPECT_EQ(3, a + b);
+}
+
+TEST(heaan_example, basic)
+{
+    long logN = 13;
+    long logQ = 65;
+    long logp = 30;
+    long logSlots = 3;
+
+    TimeUtils timeutils;
+    Context context(logN, logQ);
+    SecretKey secretKey(logN);
+    Scheme scheme(secretKey, context);
+
+    SetNumThreads(1);
+    srand(2018);
+
+    long slots = (1 << logSlots);
+
+    vector<complex<double>> mvec1 = EvaluatorUtils::randomComplexArray(slots);
+    vector<complex<double>> mvec2 = EvaluatorUtils::randomComplexArray(slots);
+    vector<complex<double>> cvec = EvaluatorUtils::randomComplexArray(slots);
+
+    vector<complex<double>> mvecAdd(slots);
+    vector<complex<double>> mvecMult(slots);
+    vector<complex<double>> mvecCMult(slots);
+
+    for (long i = 0; i < slots; i++)
+    {
+        mvecAdd[i] = mvec1[i] + mvec2[i];
+        mvecMult[i] = mvec1[i] * mvec2[i];
+        mvecCMult[i] = mvec1[i] * cvec[i];
+    }
+
+    Ciphertext cipher1 = scheme.encrypt(mvec1, logp, logQ);
+    Ciphertext cipher2 = scheme.encrypt(mvec2, logp, logQ);
+    Ciphertext addCipher = scheme.add(cipher1, cipher2);
+
+    Ciphertext multCipher = scheme.mult(cipher1, cipher2);
+    scheme.reScaleByAndEqual(multCipher, logp);
+
+    Ciphertext cmultCipher = scheme.multByConstVec(cipher1, cvec, slots, logp);
+    scheme.reScaleByAndEqual(cmultCipher, logp);
+
+    vector<complex<double>> dvecAdd = scheme.decrypt(secretKey, addCipher);
+    vector<complex<double>> dvecMult = scheme.decrypt(secretKey, multCipher);
+    vector<complex<double>> dvecCMult = scheme.decrypt(secretKey, cmultCipher);
+
+    EXPECT_EQ(1, 1);
+
+    EXPECT_TRUE((all_close(mvecAdd, dvecAdd)));
+    EXPECT_TRUE((all_close(mvecAdd, dvecAdd)));
 }
--- test/test_util.hpp
+++ test/test_util.hpp
@@ -16,6 +16,7 @@
 
 #pragma once
 
+#include <complex>
 #include <string>
 #include <vector>
 
@@ -31,3 +32,20 @@ protected:
 };
 
 std::vector<float> read_constant(const std::string filename);
+
+template <typename T>
+bool all_close(const std::vector<std::complex<T>>& a,
+               const std::vector<std::complex<T>>& b,
+               T atol = static_cast<T>(1e-5))
+{
+    assert(a.size() == b.size());
+    for (size_t i = 0; i < a.size(); ++i)
+    {
+        if ((std::abs(a[i].real() - b[i].real()) > atol) ||
+            std::abs(a[i].imag() - b[i].imag()) > atol)
+        {
+            return false;
+        }
+    }
+    return true;
+}
