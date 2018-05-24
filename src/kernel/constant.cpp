e41bee655ced25ed9c1b2765a91f89dbdcadeb65
Fabian Boemer <fabian.boemer@intel.com>
2018-05-24 13:26:01 -0700

    Added constant and result to heaan. 52 pass / 43 fail

--- src/kernel/constant.cpp
+++ src/kernel/constant.cpp
@@ -18,6 +18,7 @@
 
 #include "he_backend.hpp"
 #include "he_seal_backend.hpp"
+#include "he_heaan_backend.hpp"
 #include "kernel/constant.hpp"
 #include "ngraph/node.hpp"
 #include "ngraph/type/element_type.hpp"
@@ -32,20 +33,32 @@ void runtime::he::kernel::constant(vector<shared_ptr<he::HEPlaintext>>& out,
                                    shared_ptr<HEBackend> he_backend,
                                    size_t count)
 {
-    auto he_seal_backend = dynamic_pointer_cast<he_seal::HESealBackend>(he_backend);
-    if (!he_seal_backend)
-    {
-        throw ngraph_error("HE backend not seal type");
-    }
-
     size_t type_byte_size = type.size();
     if (out.size() != count)
     {
         throw ngraph_error("out.size() != count for constant op");
     }
-    for (size_t i = 0; i < count; ++i)
+
+    if( auto he_seal_backend = dynamic_pointer_cast<he_seal::HESealBackend>(he_backend) )
     {
-        const void* src_with_offset = (void*)((char*)data_ptr + i * type.size());
-        he_seal_backend->encode(out[i], src_with_offset, type);
+        for (size_t i = 0; i < count; ++i)
+        {
+            const void* src_with_offset = (void*)((char*)data_ptr + i * type.size());
+            he_seal_backend->encode(out[i], src_with_offset, type);
+
+        }
+    }
+    else if (auto he_heaan_backend = dynamic_pointer_cast<he_heaan::HEHeaanBackend>(he_backend))
+    {
+        for (size_t i = 0; i < count; ++i)
+        {
+            const void* src_with_offset = (void*)((char*)data_ptr + i * type.size());
+            he_heaan_backend->encode(out[i], src_with_offset, type);
+
+        }
+    }
+    else
+    {
+        throw ngraph_error("Constant backend is neither heaan nor seal.");
     }
 }
