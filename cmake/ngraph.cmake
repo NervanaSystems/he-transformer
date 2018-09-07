13420f038e3641b9229184d3a7e34b2d77ab18d4
Yixing Lao <yixing.lao@gmail.com>
2018-09-07 10:37:49 -0700

    TF intergration (#66)
    * add prebuild ngraph option
    
    * specify include and lib dir for preinstalled ngraph
    
    * keep default installation path
    
    * if PREBUILD_NGRAPH_LIB_DIR, install to the python site-packages

--- cmake/ngraph.cmake
+++ cmake/ngraph.cmake
@@ -19,26 +19,57 @@ include(ExternalProject)
 set(EXTERNAL_NGRAPH_INSTALL_DIR ${EXTERNAL_INSTALL_DIR})
 set(NGRAPH_CMAKE_PREFIX ext_ngraph)
 
-ExternalProject_Add(
-    ext_ngraph
-    GIT_REPOSITORY https://github.com/NervanaSystems/ngraph.git
-    GIT_TAG he # based on v0.7.0
-    PREFIX ${NGRAPH_CMAKE_PREFIX}
-    UPDATE_COMMAND ""
-    CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-               -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
-               -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-               -DNGRAPH_INSTALL_PREFIX=${EXTERNAL_NGRAPH_INSTALL_DIR}
-               -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-               -DNGRAPH_CPU_ENABLE=FALSE
-               -DNGRAPH_UNIT_TEST_ENABLE=FALSE
-               -DNGRAPH_TOOLS_ENABLE=FALSE
-               -DCMAKE_INSTALL_MESSAGE=LAZY
-    BUILD_BYPRODUCTS ${NGRAPH_CMAKE_PREFIX}
-    BUILD_ALWAYS 1
-)
+# PREBUILD_NGRAPH_LIB_DIR and PREBUILD_NGRAPH_INCLUDE_DIR is used for TF integration
+# - PREBUILD_NGRAPH_LIB_DIR is inside the python site-pacakges:
+#   To get the path, use `python -c "import ngraph; print(ngraph)"`
+# - PREBUILD_NGRAPH_INCLUDE_DIR is inside ngraph-tf's build directory
+#
+#   example:
+#   cmake .. -DCMAKE_BUILD_TYPE=Debug \
+#            -DPREBUILD_NGRAPH_INCLUDE_DIR=$HOME/repo/ngraph-tf/build/ngraph/ngraph_dist/include \
+#            -DPREBUILD_NGRAPH_LIB_DIR=$HOME/repo/venvs/he3/lib/python3.5/site-packages/ngraph
 
-ExternalProject_Get_Property(ext_ngraph source_dir)
-set(NGRAPH_INCLUDE_DIR ${EXTERNAL_NGRAPH_INSTALL_DIR}/include)
-set(NGRAPH_LIB_DIR ${EXTERNAL_NGRAPH_INSTALL_DIR}/lib)
-set(NGRAPH_TEST_DIR ${source_dir}/test)
+if (PREBUILD_NGRAPH_LIB_DIR)
+    set(NGRAPH_INCLUDE_DIR ${PREBUILD_NGRAPH_INCLUDE_DIR})
+    set(NGRAPH_LIB_DIR ${PREBUILD_NGRAPH_LIB_DIR})
+
+    # The only purpose here is to download the tests
+    ExternalProject_Add(
+        ext_ngraph
+        GIT_REPOSITORY https://github.com/NervanaSystems/ngraph.git
+        GIT_TAG he # based on v0.7.0
+        PREFIX ${NGRAPH_CMAKE_PREFIX}
+        UPDATE_COMMAND ""
+        BUILD_COMMAND ""
+        INSTALL_COMMAND ""
+        BUILD_ALWAYS 1
+    )
+    ExternalProject_Get_Property(ext_ngraph source_dir)
+    set(NGRAPH_TEST_DIR ${source_dir}/test)
+
+else()
+    ExternalProject_Add(
+        ext_ngraph
+        GIT_REPOSITORY https://github.com/NervanaSystems/ngraph.git
+        GIT_TAG he # based on v0.7.0
+        PREFIX ${NGRAPH_CMAKE_PREFIX}
+        UPDATE_COMMAND ""
+        CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+                -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+                -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+                -DNGRAPH_INSTALL_PREFIX=${EXTERNAL_NGRAPH_INSTALL_DIR}
+                -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+                -DNGRAPH_CPU_ENABLE=FALSE
+                -DNGRAPH_UNIT_TEST_ENABLE=FALSE
+                -DNGRAPH_TOOLS_ENABLE=FALSE
+                -DCMAKE_INSTALL_MESSAGE=LAZY
+        BUILD_BYPRODUCTS ${NGRAPH_CMAKE_PREFIX}
+        BUILD_ALWAYS 1
+    )
+
+    ExternalProject_Get_Property(ext_ngraph source_dir)
+    set(NGRAPH_INCLUDE_DIR ${EXTERNAL_NGRAPH_INSTALL_DIR}/include)
+    set(NGRAPH_LIB_DIR ${EXTERNAL_NGRAPH_INSTALL_DIR}/lib)
+    set(NGRAPH_TEST_DIR ${source_dir}/test)
+
+endif() # if (PREBUILD_NGRAPH_PATH)
