17b7ef1dbf05b6b22228655646c553dd945ab900
Yixing Lao <yixing.lao@gmail.com>
2018-09-11 05:56:49 -0700

    parsing heaan parameter working

--- src/he_heaan_parameter.hpp
+++ src/he_heaan_parameter.hpp
@@ -17,9 +17,14 @@
 #pragma once
 
 #include <cstdint>
+#include <cstdlib>
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <stdexcept>
 #include <string>
 
+#include "ngraph/log.hpp"
 #include "nlohmann/json.hpp"
 
 namespace ngraph
@@ -28,24 +33,9 @@ namespace ngraph
     {
         namespace he
         {
-            const static std::string default_heaan_parameter_str = R"(
-                {
-                    "log2_poly_modulus": 13,
-                    "log2_plain_modulus": 383,
-                    "log2_precision": 32
-                }
-            )";
-
             class HEHeaanParameter
             {
             public:
-                HEHeaanParameter(const std::string& s)
-                {
-                    nlohmann::json js = nlohmann::json::parse(s);
-                    m_log2_poly_modulus = js["log2_poly_modulus"];
-                    m_log2_plain_modulus = js["log2_plain_modulus"];
-                    m_log2_precision = js["log2_precision"];
-                }
                 HEHeaanParameter(std::uint64_t log2_poly_modulus,
                                  std::uint64_t log2_plain_modulus,
                                  std::uint64_t log2_precision)
@@ -60,8 +50,41 @@ namespace ngraph
                 std::uint64_t m_log2_precision;
             };
 
-            static HEHeaanParameter default_heaan_parameter =
-                HEHeaanParameter(default_heaan_parameter_str);
+            const static HEHeaanParameter parse_heaan_config_or_use_default()
+            {
+                try
+                {
+                    const char* config_path = std::getenv("NGRAPH_HE_HEAAN_CONFIG");
+                    if (config_path != nullptr)
+                    {
+                        // Read file to string
+                        std::ifstream f(config_path);
+                        std::stringstream ss;
+                        ss << f.rdbuf();
+                        std::string s = ss.str();
+
+                        // Parse json
+                        nlohmann::json js = nlohmann::json::parse(s);
+                        std::uint64_t log2_poly_modulus = js["log2_poly_modulus"];
+                        std::uint64_t log2_plain_modulus = js["log2_plain_modulus"];
+                        std::uint64_t log2_precision = js["log2_precision"];
+
+                        NGRAPH_INFO << "Using HEAAN config for parameters: " << config_path;
+                        return HEHeaanParameter(
+                            log2_poly_modulus, log2_plain_modulus, log2_precision);
+                    }
+                    else
+                    {
+                        NGRAPH_INFO << "Using HEAAN default parameters" << config_path;
+                        throw std::runtime_error("config_path is NULL");
+                    }
+                }
+                catch (const std::exception& e)
+                {
+                    return HEHeaanParameter(13, 383, 32);
+                }
+            }
+            static HEHeaanParameter default_heaan_parameter = parse_heaan_config_or_use_default();
         }
     }
 }
