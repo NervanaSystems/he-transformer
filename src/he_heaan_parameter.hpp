43a580a109e0fec794f0963155a0f1ade68e6645
Yixing Lao <yixing.lao@gmail.com>
2018-09-11 06:34:06 -0700

    Parse environment variable for encryption parameter config file (#67)
    * add 3rd party json lib
    
    * heaan parse default parameter
    
    * move inside
    
    * parsing heaan parameter working
    
    * seal constructor
    
    * parsing SEAL parameters
    
    * extra line

--- src/he_heaan_parameter.hpp
+++ src/he_heaan_parameter.hpp
@@ -17,6 +17,15 @@
 #pragma once
 
 #include <cstdint>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+
+#include "ngraph/log.hpp"
+#include "nlohmann/json.hpp"
 
 namespace ngraph
 {
@@ -24,18 +33,61 @@ namespace ngraph
     {
         namespace he
         {
-            struct HEHeaanParameter
+            class HEHeaanParameter
             {
+            public:
+                HEHeaanParameter(std::uint64_t log2_poly_modulus,
+                                 std::uint64_t log2_plain_modulus,
+                                 std::uint64_t log2_precision)
+                    : m_log2_poly_modulus(log2_poly_modulus)
+                    , m_log2_plain_modulus(log2_plain_modulus)
+                    , m_log2_precision(log2_precision)
+                {
+                }
+                ~HEHeaanParameter() {}
                 std::uint64_t m_log2_poly_modulus;
                 std::uint64_t m_log2_plain_modulus;
                 std::uint64_t m_log2_precision;
             };
 
-            static HEHeaanParameter default_heaan_parameter{
-                13,  // log2(poly_modulus)
-                383, // log2(plain_modulus)
-                32   // log2(precision)
-            };
+            const static HEHeaanParameter parse_heaan_config_or_use_default()
+            {
+                try
+                {
+                    const char* config_path = std::getenv("NGRAPH_HE_HEAAN_CONFIG");
+                    if (config_path != nullptr)
+                    {
+                        // Read file to string
+                        std::ifstream f(config_path);
+                        std::stringstream ss;
+                        ss << f.rdbuf();
+                        std::string s = ss.str();
+
+                        // Parse json
+                        nlohmann::json js = nlohmann::json::parse(s);
+                        std::uint64_t log2_poly_modulus = js["log2_poly_modulus"];
+                        std::uint64_t log2_plain_modulus = js["log2_plain_modulus"];
+                        std::uint64_t log2_precision = js["log2_precision"];
+
+                        NGRAPH_INFO << "Using HEAAN config for parameters: " << config_path;
+                        return HEHeaanParameter(
+                            log2_poly_modulus, log2_plain_modulus, log2_precision);
+                    }
+                    else
+                    {
+                        NGRAPH_INFO << "Using HEAAN default parameters" << config_path;
+                        throw std::runtime_error("config_path is NULL");
+                    }
+                }
+                catch (const std::exception& e)
+                {
+                    return HEHeaanParameter(13,  // m_log2_poly_modulus
+                                            383, // m_log2_plain_modulus
+                                            32   // m_log2_precision
+                                            );
+                }
+            }
+            static HEHeaanParameter default_heaan_parameter = parse_heaan_config_or_use_default();
         }
     }
 }
--- src/he_seal_parameter.hpp
+++ src/he_seal_parameter.hpp
@@ -16,14 +16,46 @@
 
 #pragma once
 
+#include <cstdint>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+
+#include "ngraph/log.hpp"
+#include "nlohmann/json.hpp"
+
 namespace ngraph
 {
     namespace runtime
     {
         namespace he
         {
-            struct HESealParameter
+            class HESealParameter
             {
+            public:
+                HESealParameter(std::uint64_t poly_modulus,
+                                std::uint64_t plain_modulus,
+                                std::uint64_t security_level,
+                                int fractional_encoder_integer_coeff_count,
+                                int fractional_encoder_fraction_coeff_count,
+                                std::uint64_t fractional_encoder_base,
+                                int evaluation_decomposition_bit_count)
+                    : m_poly_modulus(poly_modulus)
+                    , m_plain_modulus(plain_modulus)
+                    , m_security_level(security_level)
+                    , m_fractional_encoder_integer_coeff_count(
+                          fractional_encoder_integer_coeff_count)
+                    , m_fractional_encoder_fraction_coeff_count(
+                          fractional_encoder_fraction_coeff_count)
+                    , m_fractional_encoder_base(fractional_encoder_base)
+                    , m_evaluation_decomposition_bit_count(evaluation_decomposition_bit_count)
+                {
+                }
+
+                ~HESealParameter() {}
                 // SEALContext
                 // Must be 1024, 2048, 4096, 8192, 16384, or 32768, aka n
                 std::uint64_t m_poly_modulus;
@@ -40,15 +72,60 @@ namespace ngraph
                 int m_evaluation_decomposition_bit_count;
             };
 
-            static HESealParameter default_seal_parameter{
-                8192,      // poly_modulus
-                2L << 30L, // plain_modulus
-                128,       // security_level
-                64,        // fractional_encoder_integer_coeff_count
-                32,        // fractional_encoder_fraction_coeff_count
-                2,         // fractional_encoder_base
-                16         // evaluation_decomposition_bit_count
-            };
+            const static HESealParameter parse_seal_config_or_use_default()
+            {
+                try
+                {
+                    const char* config_path = std::getenv("NGRAPH_HE_SEAL_CONFIG");
+                    if (config_path != nullptr)
+                    {
+                        // Read file to string
+                        std::ifstream f(config_path);
+                        std::stringstream ss;
+                        ss << f.rdbuf();
+                        std::string s = ss.str();
+
+                        // Parse json
+                        nlohmann::json js = nlohmann::json::parse(s);
+                        std::uint64_t poly_modulus = js["poly_modulus"];
+                        std::uint64_t plain_modulus = js["plain_modulus"];
+                        std::uint64_t security_level = js["security_level"];
+                        int fractional_encoder_integer_coeff_count =
+                            js["fractional_encoder_integer_coeff_count"];
+                        int fractional_encoder_fraction_coeff_count =
+                            js["fractional_encoder_fraction_coeff_count"];
+                        std::uint64_t fractional_encoder_base = js["fractional_encoder_base"];
+                        int evaluation_decomposition_bit_count =
+                            js["evaluation_decomposition_bit_count"];
+
+                        NGRAPH_INFO << "Using SEAL config for parameters: " << config_path;
+                        return HESealParameter(poly_modulus,
+                                               plain_modulus,
+                                               security_level,
+                                               fractional_encoder_integer_coeff_count,
+                                               fractional_encoder_fraction_coeff_count,
+                                               fractional_encoder_base,
+                                               evaluation_decomposition_bit_count);
+                    }
+                    else
+                    {
+                        NGRAPH_INFO << "Using SEAL default parameters" << config_path;
+                        throw std::runtime_error("config_path is NULL");
+                    }
+                }
+                catch (const std::exception& e)
+                {
+                    return HESealParameter(8192,      // poly_modulus
+                                           2L << 30L, // plain_modulus
+                                           128,       // security_level
+                                           64,        // fractional_encoder_integer_coeff_count
+                                           32,        // fractional_encoder_fraction_coeff_count
+                                           2,         // fractional_encoder_base
+                                           16         // evaluation_decomposition_bit_count
+                                           );
+                }
+            }
+            static HESealParameter default_seal_parameter = parse_seal_config_or_use_default();
         }
     }
 }
