4d17517c7238cf9cbdc571b68d6d0a1650db81ac
Yixing Lao <yixing.lao@gmail.com>
2018-09-14 15:31:31 -0700

    fix ABI issue with ngraph

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -16,46 +16,38 @@
 
 # Enable ExternalProject CMake module
 include(ExternalProject)
-set(GTEST_PREFIX ${CMAKE_CURRENT_BINARY_DIR}/ext_gtest)
 
-#----------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 # Download and install GoogleTest ...
-#----------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 
 SET(GTEST_GIT_REPO_URL https://github.com/google/googletest.git)
 SET(GTEST_GIT_LABEL release-1.8.0)
 
-# The 'BUILD_BYPRODUCTS' argument was introduced in CMake 3.2.
+set(GTEST_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC")
+
 ExternalProject_Add(
     ext_gtest
+    PREFIX gtest
     GIT_REPOSITORY ${GTEST_GIT_REPO_URL}
     GIT_TAG ${GTEST_GIT_LABEL}
     # Disable install step
     INSTALL_COMMAND ""
     UPDATE_COMMAND ""
-    CMAKE_ARGS -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER} -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} -DCMAKE_CXX_FLAGS="-fPIC"
-    TMP_DIR "${GTEST_PREFIX}/tmp"
-    STAMP_DIR "${GTEST_PREFIX}/stamp"
-    DOWNLOAD_DIR "${GTEST_PREFIX}/download"
-    SOURCE_DIR "${GTEST_PREFIX}/src"
-    BINARY_DIR "${GTEST_PREFIX}/build"
-    INSTALL_DIR "${GTEST_PREFIX}"
-    BUILD_BYPRODUCTS "${GTEST_PREFIX}/build/googlemock/gtest/libgtest.a"
+    CMAKE_ARGS -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+               -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+               -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+               -DCMAKE_CXX_FLAGS=${GTEST_CXX_FLAGS}
+    EXCLUDE_FROM_ALL TRUE
+    BUILD_ALWAYS 1
 )
 
-#----------------------------------------------------------------------------------------------------------
 
-get_filename_component(
-    GTEST_INCLUDE_DIR
-    "${GTEST_PREFIX}/src/googletest/include"
-    ABSOLUTE)
+#------------------------------------------------------------------------------
 
-# Create a libgtest target to be used as a dependency by test programs
-add_library(libgtest IMPORTED STATIC GLOBAL)
+ExternalProject_Get_Property(ext_gtest SOURCE_DIR BINARY_DIR)
+
+add_library(libgtest INTERFACE)
 add_dependencies(libgtest ext_gtest)
-
-# Set libgtest properties
-set_target_properties(libgtest PROPERTIES
-    "IMPORTED_LOCATION" "${GTEST_PREFIX}/build/googlemock/gtest/libgtest.a"
-    "IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}"
-)
+target_include_directories(libgtest SYSTEM INTERFACE ${SOURCE_DIR}/googletest/include)
+target_link_libraries(libgtest INTERFACE ${BINARY_DIR}/googlemock/gtest/libgtest.a)
