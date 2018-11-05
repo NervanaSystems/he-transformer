62e280af06e86194b0003ecf19516d4e7762fdfa
Fabian Boemer <fabian.boemer@intel.com>
2018-11-05 11:39:02 -0800

    Started docker build; seg-fault on convolution of cryptonets testing

--- docker/build_ngraph_he_and_test.sh
+++ docker/build_ngraph_he_and_test.sh
@@ -4,26 +4,13 @@ set -e
 # set -u  # Cannot use set -u, as activate below relies on unbound variables
 set -o pipefail
 
-# Debugging to verify builds on CentOS 7.4 and Ubuntu 16.04
-if [ -f "/etc/centos-release" ]; then
-    cat /etc/centos-release
-fi
-
-if [ -f "/etc/lsb-release" ]; then
-    cat /etc/lsb-release
-fi
-
-uname -a
-cat /etc/os-release || true
-
+# If proxy settings are detected in the environment, make sure they are
+# included on the docker-build command-line.
 if [ ! -z "${http_proxy}" ] ; then
     DOCKER_HTTP_PROXY="--build-arg http_proxy=${http_proxy}"
 else
     DOCKER_HTTP_PROXY=' '
 fi
-
-echo 'https proxy' ${https_proxy}
-echo 'http proxy' ${http_proxy}
 
 if [ ! -z "${https_proxy}" ] ; then
     DOCKER_HTTPS_PROXY="--build-arg https_proxy=${https_proxy}"
@@ -31,15 +18,52 @@ else
     DOCKER_HTTPS_PROXY=' '
 fi
 
-DFILE="Dockerfile.ngraph-he"
+uname -a
+cat /etc/os-release || true
 
-CONTEXT='.'
+echo ' '
+echo 'Contents of /home:'
+ls -la /home
+echo ' '
 
-# build the docker base image
-docker build  --rm=true \
-       ${DOCKER_HTTP_PROXY} ${DOCKER_HTTPS_PROXY} \
-       -f="${DFILE}" \
-       ${CONTEXT}
+# Test c++ integration
+mkdir build
+cd build
+cmake ..
+make -j22
+./test/unit-test
+NGRAPH_HE_SEAL_CONFIG=../test/model/he_seal_ckks_config_13.json ./test/cryptonets_benchmark
+NGRAPH_HE_SEAL_CONFIG=../test/model/he_seal_ckks_config_14.json ./test/cryptonets_benchmark
 
-    echo 'Docker image build completed'
+# Test python integration
+cd ..
+rm -rf build
+mkdir build
+mkdir -p ~/venvs
+virtualenv ~/venvs/he3 -p python3
+source ~/venvs/he3/bin/activate
+cd build
+cmake .. -DENABLE_TF=on
+make -j22
+make install
+
+# Test c++ unit-tests under python
+./test/unit-test
+
+# Test python unit-test
+cd ../examples
+NGRAPH_TF_BACKEND=HE:SEAL:BFV python axpy.py
+NGRAPH_TF_BACKEND=HE:SEAL:CKKS python axpy.py
+
+# Test cryptonets under python
+cd cryptonets
+NGRAPH_HE_SEAL_CONFIG=../../test/model/he_seal_ckks_config_13.json NGRAPH_TF_BACKEND=HE:SEAL:CKKS python test.py --batch_size=1
+
+
+
+
+
+
+
+
 
