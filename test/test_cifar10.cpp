c241eb2528956adb3a0e889bb7a81cd3cff69555
Fabian Boemer <fabian.boemer@intel.com>
2018-12-06 15:34:59 -0800

    Added untrained cifar10 model; invalid argument (NNT) when running

---
+++ test/test_cifar10.cpp
@@ -0,0 +1,182 @@
+//*****************************************************************************
+// Copyright 2018 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//*****************************************************************************
+
+#include <assert.h>
+#include <algorithm>
+
+#include "seal/ckks/he_seal_ckks_backend.hpp"
+#include "test_util.hpp"
+#include "util/test_tools.hpp"
+
+#include "ngraph/ngraph.hpp"
+#include "ngraph/util.hpp"
+
+#include "util/all_close.hpp"
+#include "util/ndarray.hpp"
+#include "util/test_control.hpp"
+#include "util/test_tools.hpp"
+
+using namespace std;
+using namespace ngraph;
+
+static string s_manifest = "";
+
+static void run_cifar10_benchmark(string backend_name, size_t batch_size = 1) {
+  if (backend_name == "INTERPRETER") {
+    assert(batch_size == 1);
+  }
+
+  assert(batch_size == 1);
+
+  auto backend = runtime::Backend::create(backend_name);
+  auto he_backend = dynamic_cast<runtime::he::HEBackend*>(backend.get());
+  if (he_backend) {
+    // Avoid multiplication by plaintext 0 when data is plaintext.
+    bool encrypt_data = (std::getenv("NGRAPH_ENCRYPT_DATA") != nullptr);
+    if (!encrypt_data) {
+      he_backend->set_optimized_add(true);
+      he_backend->set_optimized_mult(true);
+    }
+  }
+
+  /* vector<float> x = read_binary_constant(
+       file_util::path_join(HE_SERIALIZED_ZOO, "x_test_4096.bin"),
+       batch_size * 784);
+   vector<float> y = read_binary_constant(
+       file_util::path_join(HE_SERIALIZED_ZOO, "y_test_4096.bin"),
+       batch_size * 10);
+       */
+
+  vector<float> x(batch_size, 0.1);
+  vector<float> y(batch_size * 10, 0);
+
+  for (auto elem : x) {
+    NGRAPH_INFO << "elem, " << elem;
+  }
+
+  // Global stop watch
+  stopwatch sw_global;
+  sw_global.start();
+
+  // Load graph
+  stopwatch sw_load_model;
+  sw_load_model.start();
+  const string filename = "cifar10_" + to_string(batch_size);
+  const string json_path =
+      file_util::path_join(HE_SERIALIZED_ZOO, filename + ".json");
+
+  NGRAPH_INFO << "Reading file " << json_path;
+  const string json_string = file_util::read_file_to_string(json_path);
+  shared_ptr<Function> f = deserialize(json_string);
+  NGRAPH_INFO << "Deserialize graph";
+  NGRAPH_INFO << "x size " << x.size();
+  NGRAPH_INFO << "Inputs loaded";
+  sw_load_model.stop();
+  NGRAPH_INFO << "sw_load_model: " << sw_load_model.get_milliseconds() << "ms";
+
+  // Create input tensor and copy tensors; create output tensors
+  stopwatch sw_encrypt_input;
+  sw_encrypt_input.start();
+  auto parameters = f->get_parameters();
+  vector<shared_ptr<runtime::Tensor>> parameter_tvs;
+  for (const auto& parameter : parameters) {
+    auto& shape = parameter->get_shape();
+    auto& type = parameter->get_element_type();
+
+    std::shared_ptr<runtime::Tensor> parameter_cipher_tv =
+        backend->create_tensor(type, shape);
+
+    NGRAPH_INFO << "Creating input shape: " << join(shape, "x");
+
+    if (shape == Shape{batch_size, 24, 24, 3}) {
+      NGRAPH_INFO << "Copying " << shape_size(shape) << " elements";
+
+      copy_data(parameter_cipher_tv, x);
+      parameter_tvs.push_back(parameter_cipher_tv);
+    } else {
+      throw ngraph_error("Invalid shape " + to_string(shape_size(shape)));
+    }
+  }
+
+  auto results = f->get_results();
+  vector<shared_ptr<runtime::Tensor>> result_tvs;
+  for (const auto& result : results) {
+    auto& shape = result->get_shape();
+    auto& type = result->get_element_type();
+    NGRAPH_INFO << "Creating output shape: " << join(shape, "x");
+
+    result_tvs.push_back(backend->create_tensor(type, shape));
+  }
+  sw_encrypt_input.stop();
+  NGRAPH_INFO << "sw_encrypt_input: " << sw_encrypt_input.get_milliseconds()
+              << "ms";
+
+  // Run model
+  NGRAPH_INFO << "calling function";
+  stopwatch sw_run_model;
+  sw_run_model.start();
+  backend->call(f, result_tvs, parameter_tvs);
+  sw_run_model.stop();
+  NGRAPH_INFO << "sw_run_model: " << sw_run_model.get_milliseconds() << "ms";
+
+  // Decrypt output
+  stopwatch sw_decrypt_output;
+  sw_decrypt_output.start();
+  auto result = (backend_name == "INTERPRETER")
+                    ? read_vector<float>(result_tvs[0])
+                    : generalized_read_vector<float>(result_tvs[0]);
+  sw_decrypt_output.stop();
+  NGRAPH_INFO << "sw_decrypt_output: " << sw_decrypt_output.get_milliseconds()
+              << "ms";
+
+  // Stop global stop watch
+  sw_global.stop();
+  NGRAPH_INFO << "sw_global: " << sw_global.get_milliseconds() << "ms";
+
+  // Check prediction vs ground truth
+  vector<int> y_gt_label = batched_argmax(y);
+  vector<int> y_predicted_label = batched_argmax(result);
+
+  size_t error_count = 0;
+  for (size_t i = 0; i < y_gt_label.size(); ++i) {
+    if (y_gt_label[i] != y_predicted_label[i]) {
+      // NGRAPH_INFO << "index " << i << " y_gt_label != y_predicted_label: " <<
+      // y_gt_label[i]
+      //             << " != " << y_predicted_label[i];
+      error_count++;
+    }
+  }
+  NGRAPH_INFO << "Error count " << error_count << " of " << batch_size
+              << " elements.";
+  float accuracy = 1.f - (float)(error_count) / batch_size;
+  NGRAPH_INFO << "Accuracy: " << accuracy;
+
+  if (batch_size > 100) {
+    assert(accuracy > 0.97);
+  }
+
+  // Print results
+  NGRAPH_INFO << "[Summary]";
+  NGRAPH_INFO << "sw_load_model: " << sw_load_model.get_milliseconds() << "ms";
+  NGRAPH_INFO << "sw_encrypt_input: " << sw_encrypt_input.get_milliseconds()
+              << "ms";
+  NGRAPH_INFO << "sw_run_model: " << sw_run_model.get_milliseconds() << "ms";
+  NGRAPH_INFO << "sw_decrypt_output: " << sw_decrypt_output.get_milliseconds()
+              << "ms";
+  NGRAPH_INFO << "sw_global: " << sw_global.get_milliseconds() << "ms";
+};
+
+NGRAPH_TEST(CIFAR10, CKKS_1) { run_cifar10_benchmark("HE:SEAL:CKKS", 1); }
