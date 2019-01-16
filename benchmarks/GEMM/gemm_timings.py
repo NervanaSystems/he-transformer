0642b59ccc39614376152eea73fb77d35465442e
Fabian Boemer <fabian.boemer@intel.com>
2019-01-16 13:09:00 -0800

    Newer GEMM results

--- benchmarks/GEMM/gemm_timings.py
+++ benchmarks/GEMM/gemm_timings.py
@@ -14,21 +14,24 @@
 #  limitations under the License.
 # ==============================================================================
 
-import ngraph
+import ngraph_bridge
 import numpy as np
 import tensorflow as tf
 import time
 import sys
 import argparse
 
+
 def gemm_trial(n, p_ones=0, fname="./results.txt"):
     # Pick values not in {-1, 0, 1} for fair comparison
-    ra = np.float32(np.random.choice([-5,-4,-3,-2,2,3,4,5], size=(n,n)))
-    rb = np.float32(np.random.choice([-5,-4,-3,-2,2,3,4,5], size=(n,n)))
+    ra = np.float32(
+        np.random.choice([-5, -4, -3, -2, 2, 3, 4, 5], size=(n, n)))
+    rb = np.float32(
+        np.random.choice([-5, -4, -3, -2, 2, 3, 4, 5], size=(n, n)))
 
     # randomly set entries to 1
     n_ones = int(p_ones * n * n)
-    one_indices = np.random.choice( n * n, n_ones, replace=False)
+    one_indices = np.random.choice(n * n, n_ones, replace=False)
     for i in one_indices:
         row = i // n
         col = i % n
@@ -46,21 +49,25 @@ def gemm_trial(n, p_ones=0, fname="./results.txt"):
     with tf.Session() as sess:
         t0 = time.time()
         f_val = sess.run(f, feed_dict={b: rb, c: 3.})
-        t1 = time.time()-t0
+        t1 = time.time() - t0
         print("Result: ", f_val)
         print("Time: ", t1)
         fp = open(fname, 'a')
-        fp.write('%d, %f, %f, %f\n' % (n, p_zeros, p_ones, t1))
+        fp.write('%d, %f, %f\n' % (n, p_ones, t1))
         fp.close()
+
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description='Rum GEMM timing experiments for he-transformer.')
-    parser.add_argument('--out', help='Name of output CSV file', default='./results.txt')
+    parser = argparse.ArgumentParser(
+        description='Rum GEMM timing experiments for he-transformer.')
+    parser.add_argument(
+        '--out', help='Name of output CSV file', default='./results.txt')
     args = parser.parse_args()
 
     fname = args.out
 
-    for n in [45, 40, 35, 30, 25, 20, 15, 10, 5]:
+    ns = [5, 10, 15, 20, 25, 30, 35, 40, 45]
+    for n in ns:
         for p_ones in [0, 0.5, 0.8]:
             gemm_trial(n=n, p_ones=p_ones, fname=fname)
--- benchmarks/GEMM/run_timings.sh
+++ benchmarks/GEMM/run_timings.sh
@@ -1,3 +1,13 @@
-OMP_NUM_THREADS=1 NGRAPH_HE_SEAL_CONFIG=he_seal_ckks_config_13.json NGRAPH_TF_BACKEND=HE:SEAL:CKKS python gemm_timings.py --out=./gemm_13_results.txt
-OMP_NUM_THREADS=1 NGRAPH_HE_SEAL_CONFIG=he_seal_ckks_config_14.json NGRAPH_TF_BACKEND=HE:SEAL:CKKS python gemm_timings.py --out=./gemm_14_results.txt
+OMP_NUM_THREADS=1 \
+  NGRAPH_HE_SEAL_CONFIG=he_seal_ckks_config_13.json \
+  NGRAPH_TF_BACKEND=HE_SEAL_CKKS \
+  NGRAPH_ENCRYPT_DATA=1 \
+  NGRAPH_OPTIMIZED_MULT=1 \
+  python gemm_timings.py --out=./gemm_13_results.txt
 
+OMP_NUM_THREADS=1 \
+  NGRAPH_HE_SEAL_CONFIG=he_seal_ckks_config_14.json \
+  NGRAPH_TF_BACKEND=HE_SEAL_CKKS \
+  NGRAPH_ENCRYPT_DATA=1 \
+  NGRAPH_OPTIMIZED_MULT=1 \
+  python gemm_timings.py --out=./gemm_14_results.txt
