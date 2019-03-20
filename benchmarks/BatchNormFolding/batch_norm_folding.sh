5c856b773fe9e289ef818c7781a4f64573a487d6
Fabian Boemer <fabian.boemer@intel.com>
2019-03-20 08:01:47 -0700

    Added no-bn runtimes on bs=1; added CIFAR10 new runtimes

--- benchmarks/BatchNormFolding/batch_norm_folding.sh
+++ benchmarks/BatchNormFolding/batch_norm_folding.sh
@@ -12,5 +12,3 @@ do
   echo "16384 Trial $i"
   OMP_NUM_THREADS=1 NGRAPH_HE_SEAL_CONFIG=./bn_config_16384.json ../../build/test/unit-test --gtest_filter="HE_SEAL_CKKS.batch_norm_fusion_he_large" > results/exp16384_${i}.txt
 done
-
-
