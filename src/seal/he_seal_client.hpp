0e081033f065a914a2c2322816517d335ff7d43c
Fabian Boemer <fabian.boemer@intel.com>
2019-04-02 09:51:52 -0700

    Moved seal client to cpp/hpp

--- src/seal/he_seal_client.hpp
+++ src/seal/he_seal_client.hpp
@@ -14,13 +14,14 @@
 // limitations under the License.
 //*****************************************************************************
 
+#pragma once
+
 #include <boost/asio.hpp>
 #include <iostream>
 #include <memory>
 #include <string>
 #include <vector>
 
-#include "seal/he_seal_client.hpp"
 #include "seal/he_seal_util.hpp"
 #include "seal/seal.h"
 #include "tcp/tcp_client.hpp"
@@ -28,20 +29,46 @@
 
 using namespace ngraph;
 
-ngraph::runtime::he::HESealClient::HESealClient(
-    boost::asio::io_context& io_context,
-    const tcp::resolver::results_type& endpoints, std::vector<float> inputs);
+namespace ngraph {
+namespace runtime {
+namespace he {
+class HESealClient {
+ public:
+  HESealClient(boost::asio::io_context& io_context,
+               const tcp::resolver::results_type& endpoints,
+               std::vector<float> inputs);
 
-void ngraph::runtime::he::HESealClient::set_seal_context();
+  ~HESealClient() = default;
 
-void ngraph::runtime::he::HESealClient::handle_message(
-    const runtime::he::TCPMessage& message);
+  void set_seal_context();
 
-void ngraph::runtime::he::HESealClient::write_message(
-    const runtime::he::TCPMessage& message);
+  void handle_message(const runtime::he::TCPMessage& message);
 
-bool ngraph::runtime::he::HESealClient::is_done();
+  void write_message(const runtime::he::TCPMessage& message);
 
-std::vector<float> ngraph::runtime::he::HESealClient::get_results();
+  bool is_done();
 
-void ngraph::runtime::he::HESealClient::close_connection();
+  std::vector<float> get_results();
+
+  void close_connection();
+
+ private:
+  std::shared_ptr<TCPClient> m_tcp_client;
+  seal::EncryptionParameters m_encryption_params{seal::scheme_type::CKKS};
+  std::shared_ptr<seal::PublicKey> m_public_key;
+  std::shared_ptr<seal::SecretKey> m_secret_key;
+  std::shared_ptr<seal::SEALContext> m_context;
+  std::shared_ptr<seal::Encryptor> m_encryptor;
+  std::shared_ptr<seal::CKKSEncoder> m_ckks_encoder;
+  std::shared_ptr<seal::Decryptor> m_decryptor;
+  std::shared_ptr<seal::Evaluator> m_evaluator;
+  std::shared_ptr<seal::KeyGenerator> m_keygen;
+  std::shared_ptr<seal::RelinKeys> m_relin_keys;
+  double m_scale;
+  bool m_is_done;
+  std::vector<float> m_inputs;   // Function inputs
+  std::vector<float> m_results;  // Function outputs
+};
+}  // namespace he
+}  // namespace runtime
+}  // namespace ngraph
