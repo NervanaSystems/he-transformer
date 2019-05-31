280746daa8c8a8d53f1c79ee24e6dfd2729e574d
Fabian Boemer <fabian.boemer@intel.com>
2019-05-31 15:42:52 -0700

    Seems to work

--- cmake/boost.cmake
+++ cmake/boost.cmake
@@ -16,13 +16,17 @@
 
 include(ExternalProject)
 
-SET(BOOST_REPO_URL https://github.com/boostorg/boost)
+SET(BOOST_ASIO_REPO_URL https://github.com/boostorg/asio)
+SET(BOOST_SYSTEM_REPO_URL https://github.com/boostorg/system)
+SET(BOOST_CONFIG_REPO_URL https://github.com/boostorg/config)
 SET(BOOST_GIT_LABEL boost-1.69.0)
 
+add_library(libboost INTERFACE)
+
 ExternalProject_Add(
-    ext_boost
+    ext_boost_asio
     PREFIX boost
-    GIT_REPOSITORY ${BOOST_REPO_URL}
+    GIT_REPOSITORY ${BOOST_ASIO_REPO_URL}
     GIT_TAG ${BOOST_GIT_LABEL}
     # Disable install step
     CONFIGURE_COMMAND ""
@@ -32,14 +36,47 @@ ExternalProject_Add(
     EXCLUDE_FROM_ALL TRUE
     )
 
+ExternalProject_Get_Property(ext_boost_asio SOURCE_DIR)
+message("boost asio SOURCE_DIR ${SOURCE_DIR}")
+target_include_directories(libboost SYSTEM INTERFACE ${SOURCE_DIR}/include)
+set(BOOST_HEADERS_PATH ${BOOST_HEADERS_PATH} ${SOURCE_DIR})
 
+ExternalProject_Add(
+    ext_boost_system
+    PREFIX boost
+    GIT_REPOSITORY ${BOOST_SYSTEM_REPO_URL}
+    GIT_TAG ${BOOST_GIT_LABEL}
+    # Disable install step
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+    UPDATE_COMMAND ""
+    EXCLUDE_FROM_ALL TRUE
+    BUILDE_IN_SOURCE 1
+    )
+ExternalProject_Get_Property(ext_boost_system SOURCE_DIR)
+message("boost system SOURCE_DIR ${SOURCE_DIR}")
+target_include_directories(libboost SYSTEM INTERFACE ${SOURCE_DIR}/include)
+set(BOOST_HEADERS_PATH ${BOOST_HEADERS_PATH} ${SOURCE_DIR})
 
+ExternalProject_Add(
+    ext_boost_config
+    PREFIX boost
+    GIT_REPOSITORY ${BOOST_CONFIG_REPO_URL}
+    GIT_TAG ${BOOST_GIT_LABEL}
+    # Disable install step
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+    UPDATE_COMMAND ""
+    EXCLUDE_FROM_ALL TRUE
+    )
+ExternalProject_Get_Property(ext_boost_config SOURCE_DIR)
+message("boost config SOURCE_DIR ${SOURCE_DIR}")
+target_include_directories(libboost SYSTEM INTERFACE ${SOURCE_DIR}/include)
 
-#find_package(Boost 1.69)
-#if(Boost_FOUND)
-#  message("Boost_INCLUDE_DIRS ${Boost_INCLUDE_DIRS}")
-#  include_directories(${Boost_INCLUDE_DIRS})
-#else()
-#  message(FATAL_ERROR "Boost not found")
-#endif()
-#add_library(boost INTERFACE)
+set(BOOST_HEADERS_PATH ${BOOST_HEADERS_PATH} ${SOURCE_DIR})
+
+message("BOOST_HEADERS_PATH ${BOOST_HEADERS_PATH}")
+
+add_dependencies(libboost ext_boost_asio ext_boost_system ext_boost_config)
