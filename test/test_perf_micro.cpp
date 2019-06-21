df502a691ed96573ded4dfe07a98c3c05932dad6
Fabian Boemer <fabian.boemer@intel.com>
2019-06-21 09:48:28 -0700

    Add/mult tests

--- test/test_perf_micro.cpp
+++ test/test_perf_micro.cpp
@@ -31,7 +31,7 @@ using namespace seal;
 static string s_manifest = "${MANIFEST}";
 
 TEST(perf_micro, encode) {
-  int test_count = 1000;
+  int test_count = 100;
 
   auto perf_test = [&test_count](size_t poly_modulus_degree,
                                  const std::vector<int>& coeff_modulus_bits) {
@@ -39,14 +39,29 @@ TEST(perf_micro, encode) {
     chrono::nanoseconds time_seal_encode_sum(0);
     chrono::nanoseconds time_he_encode_sum(0);
 
+    chrono::nanoseconds time_seal_multiply_plain_sum(0);
+    chrono::nanoseconds time_he_multiply_plain_sum(0);
+
+    chrono::nanoseconds time_seal_add_plain_sum(0);
+    chrono::nanoseconds time_he_add_plain_sum(0);
+
+    // Seal setup
     EncryptionParameters parms(scheme_type::CKKS);
 
     parms.set_poly_modulus_degree(poly_modulus_degree);
     parms.set_coeff_modulus(
         CoeffModulus::Create(poly_modulus_degree, coeff_modulus_bits));
+
     auto context = SEALContext::Create(parms);
     CKKSEncoder encoder(context);
+    KeyGenerator keygen(context);
+    auto secret_key = keygen.secret_key();
+    auto public_key = keygen.public_key();
+    Encryptor encryptor(context, public_key);
+    Decryptor decryptor(context, secret_key);
+    Evaluator evaluator(context);
 
+    // he-transformer setup
     auto he_parms = HESealEncryptionParameters("HE_SEAL", poly_modulus_degree,
                                                128, coeff_modulus_bits);
     auto he_seal_backend = HESealBackend(he_parms);
@@ -58,48 +73,113 @@ TEST(perf_micro, encode) {
 
       Plaintext plain(seal_capacity);
       double input{1.23};
-      double scale = pow(2.0, 30);
+      double scale = pow(2.0, 22);
       std::vector<std::uint64_t> he_plain;
       auto parms_id = context->first_parms_id();
 
-      // SEAL encoder
-      time_start = chrono::high_resolution_clock::now();
-      encoder.encode(input, scale, plain, pool);
-      time_end = chrono::high_resolution_clock::now();
-      time_seal_encode_sum +=
-          chrono::duration_cast<chrono::nanoseconds>(time_end - time_start);
+      // [Encoding]
+      {
+        // SEAL encoder
+        time_start = chrono::high_resolution_clock::now();
+        encoder.encode(input, scale, plain, pool);
+        time_end = chrono::high_resolution_clock::now();
+        time_seal_encode_sum +=
+            chrono::duration_cast<chrono::nanoseconds>(time_end - time_start);
 
-      // HE encoder
-      time_start = chrono::high_resolution_clock::now();
-      ngraph::he::encode(input, scale, parms_id, he_plain, he_seal_backend,
-                         pool);
-      time_end = chrono::high_resolution_clock::now();
-      time_he_encode_sum +=
-          chrono::duration_cast<chrono::nanoseconds>(time_end - time_start);
+        // HE encoder
+        time_start = chrono::high_resolution_clock::now();
+        ngraph::he::encode(input, scale, parms_id, he_plain, he_seal_backend,
+                           pool);
+        time_end = chrono::high_resolution_clock::now();
+        time_he_encode_sum +=
+            chrono::duration_cast<chrono::nanoseconds>(time_end - time_start);
 
-      if (test_run == 0) {
-        NGRAPH_INFO << "seal::Plaintext capacity " << plain.capacity();
-        auto he_capacity = sizeof(std::uint64_t) * he_plain.size();
-        NGRAPH_INFO << "he plaintext capacity " << he_capacity;
-        NGRAPH_INFO << "Memmory improvement: "
-                    << plain.capacity() / float(he_capacity);
+        if (test_run == 0) {
+          NGRAPH_INFO << "seal::Plaintext capacity " << plain.capacity();
+          auto he_capacity = sizeof(std::uint64_t) * he_plain.size();
+          NGRAPH_INFO << "he plaintext capacity " << he_capacity;
+          NGRAPH_INFO << "Memmory improvement: "
+                      << plain.capacity() / float(he_capacity) << "\n";
+        }
       }
+
+      // Plaintext multiplication
+      if (coeff_modulus_bits.size() > 2) {
+        Ciphertext encrypted(context);
+        encryptor.encrypt(plain, encrypted);
+
+        // SEAL
+        time_start = chrono::high_resolution_clock::now();
+        evaluator.multiply_plain_inplace(encrypted, plain, pool);
+        time_end = chrono::high_resolution_clock::now();
+        time_seal_multiply_plain_sum +=
+            chrono::duration_cast<chrono::microseconds>(time_end - time_start);
+
+        // HE
+        time_start = chrono::high_resolution_clock::now();
+        multiply_plain_inplace(encrypted, input, he_seal_backend, pool);
+        time_end = chrono::high_resolution_clock::now();
+        time_he_multiply_plain_sum +=
+            chrono::duration_cast<chrono::microseconds>(time_end - time_start);
+      }
+
+      // Plaintext addition
+      Ciphertext encrypted(context);
+      encryptor.encrypt(plain, encrypted);
+
+      // SEAL
+      time_start = chrono::high_resolution_clock::now();
+      evaluator.add_plain_inplace(encrypted, plain);
+      time_end = chrono::high_resolution_clock::now();
+      time_seal_add_plain_sum +=
+          chrono::duration_cast<chrono::microseconds>(time_end - time_start);
+
+      // HE
+      time_start = chrono::high_resolution_clock::now();
+      add_plain_inplace(encrypted, input, he_seal_backend);
+      time_end = chrono::high_resolution_clock::now();
+      time_he_add_plain_sum +=
+          chrono::duration_cast<chrono::microseconds>(time_end - time_start);
     }
 
     auto time_seal_encode_avg = time_seal_encode_sum.count() / test_count;
     auto time_he_encode_avg = time_he_encode_sum.count() / test_count;
 
+    auto time_seal_multiply_plain_avg =
+        time_seal_multiply_plain_sum.count() / test_count;
+    auto time_he_multiply_plain_avg =
+        time_he_multiply_plain_sum.count() / test_count - time_he_encode_avg;
+
+    auto time_seal_add_plain_avg = time_seal_add_plain_sum.count() / test_count;
+    auto time_he_add_plain_avg =
+        time_he_add_plain_sum.count() / test_count - time_he_encode_avg;
+
     NGRAPH_INFO << "time_seal_encode_avg (ns) " << time_seal_encode_avg;
     NGRAPH_INFO << "time_he_encode_avg (ns) " << time_he_encode_avg;
     NGRAPH_INFO << "Runtime improvement: "
-                << (time_seal_encode_avg / float(time_he_encode_avg));
+                << (time_seal_encode_avg / float(time_he_encode_avg)) << "\n";
+
+    NGRAPH_INFO << "time_seal_multiply_plain_avg (ns) "
+                << time_seal_multiply_plain_avg;
+    NGRAPH_INFO << "time_he_multiply_plain_avg (ns) "
+                << time_he_multiply_plain_avg;
+    NGRAPH_INFO << "Runtime improvement: "
+                << (time_seal_multiply_plain_avg /
+                    float(time_he_multiply_plain_avg))
+                << "\n";
+
+    NGRAPH_INFO << "time_seal_add_plain_avg (ns) " << time_seal_add_plain_avg;
+    NGRAPH_INFO << "time_he_add_plain_avg (ns) " << time_he_add_plain_avg;
+    NGRAPH_INFO << "Runtime improvement: "
+                << (time_seal_add_plain_avg / float(time_he_add_plain_avg))
+                << "\n";
   };
 
   std::vector<size_t> poly_modulus_degrees{4096, 8192, 16384};
 
   // Bit-widths of default BFV parameters
   std::vector<std::vector<int>> coeff_modulus_bits{
-      {36, 37}, {43, 43, 44, 44}, {48, 48, 48, 49, 49, 49, 49, 49, 49}};
+      {36, 37}, {30, 30, 30, 30}, {30, 30, 30, 30, 30, 30, 30, 30, 30}};
 
   for (size_t parm_ind = 0; parm_ind < poly_modulus_degrees.size();
        ++parm_ind) {
