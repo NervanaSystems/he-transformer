621b8bd1ca97ac1f7882c05092703e23bc235a46
Fabian Boemer <fabian.boemer@intel.com>
2019-06-24 16:13:36 -0700

    First pass at MNIST-Cryptonets results

--- results/mnist-cryptonets/run_experiments.sh
+++ results/mnist-cryptonets/run_experiments.sh
@@ -15,20 +15,27 @@ do
   NGRAPH_HE_SEAL_CONFIG=../../test/model/he_seal_ckks_config_N13_L7.json \
   NGRAPH_TF_BACKEND=HE_SEAL \
   python test.py --batch_size=4096 > ../../results/mnist-cryptonets/results/best_${i}.txt
+done
 
-  # 1 thread
+
+# 1 thread
+for i in {1..10}
+do
   OMP_NUM_THREADS=1 NGRAPH_ENCRYPT_DATA=1 \
   NGRAPH_HE_SEAL_CONFIG=../../test/model/he_seal_ckks_config_N13_L7.json \
   NGRAPH_TF_BACKEND=HE_SEAL \
   python test.py --batch_size=4096 > ../../results/mnist-cryptonets/results/OMP1_${i}.txt
+done
 
-  # naive rescaling
+# naive rescaling
+for i in {1..10}
+do
   OMP_NUM_THREADS=1 \
   NGRAPH_ENCRYPT_DATA=1 \
   NAIVE_RESCALING=1 \
   NGRAPH_HE_SEAL_CONFIG=../../test/model/he_seal_ckks_config_N13_L7.json \
   NGRAPH_TF_BACKEND=HE_SEAL \
   python test.py --batch_size=4096 > ../../results/mnist-cryptonets/results/naive_recsaling_${i}.txt
-
 done
+
 cd -
