ba9145a92a5cbec9ffd681edcfc16fb6f1e89f4b
Fabian Boemer <fabian.boemer@intel.com>
2019-06-27 11:09:36 -0700

    run experiments real working

--- results/mobilenet_results/run_experiments_real.sh
+++ results/mobilenet_results/run_experiments_real.sh
@@ -18,6 +18,7 @@ do
 
   (OMP_NUM_THREADS=56 \
   NGRAPH_ENABLE_CLIENT=1 \
+  STOP_CONST_FOLD=1 \
   NGRAPH_ENCRYPT_DATA=1 \
   NGRAPH_TF_BACKEND=HE_SEAL \
   NGRAPH_HE_SEAL_CONFIG=../../test/model/he_seal_ckks_config_N12_L4.json \
@@ -33,7 +34,5 @@ do
   --batch_size=2048 \
   --data_dir=$DATA_DIR >> $outfile
 
-
 done
-
 cd -
