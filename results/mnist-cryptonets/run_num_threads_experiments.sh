069b3eb0ba680b10f489eb10727673df0eea42b4
Fabian Boemer <fabian.boemer@intel.com>
2019-07-24 07:12:38 -0700

    Added more threaded results

--- results/mnist-cryptonets/run_num_threads_experiments.sh
+++ results/mnist-cryptonets/run_num_threads_experiments.sh
@@ -9,12 +9,13 @@ echo $pwd
 outfolder=../../results/mnist-cryptonets/results
 
 # tune OMP_NUM_THREADS
-for nt in {56,32,16,8,4,2,1} #1, 2, 4, 8, 16, 32, 56}
+for nt in {24,40,48,64,72,80,88,96,104,112} # {1,56,32,16,8,4,2,1} #1, 2, 4, 8, 16, 32, 56}
   do
   for i in {1..10}
   do
     outfile=$outfolder/best_nt${nt}_${i}.txt
     OMP_NUM_THREADS=${nt} \
+    NGRAPH_VOPS=total \
     NGRAPH_ENCRYPT_DATA=1 \
     NGRAPH_HE_SEAL_CONFIG=../../test/model/he_seal_ckks_config_N13_L7.json \
     NGRAPH_TF_BACKEND=HE_SEAL \
