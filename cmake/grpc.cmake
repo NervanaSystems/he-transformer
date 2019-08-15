fda3a774148ac93af946822c129ef3d4efd2054f
Fabian Boemer <fabian.boemer@intel.com>
2019-08-15 10:57:53 -0700

    Greeter cleint class compiles

--- cmake/grpc.cmake
+++ cmake/grpc.cmake
@@ -22,7 +22,9 @@ ExternalProject_Add(ext_grpc
                     PREFIX grpc
                     GIT_REPOSITORY https://github.com/grpc/grpc.git
                     GIT_TAG v1.22.0
-                    INSTALL_COMMAND "")
+                    INSTALL_COMMAND ""
+                    UPDATE_COMMAND "")
+message("PROJECT_SOURCE_DIR ${PROJECT_SOURCE_DIR}")
 
 ExternalProject_Get_Property(ext_grpc SOURCE_DIR)
 ExternalProject_Get_Property(ext_grpc BINARY_DIR)
@@ -34,3 +36,65 @@ set_target_properties(libgrpc
 
 target_include_directories(libgrpc SYSTEM INTERFACE ${SOURCE_DIR}/include)
 add_dependencies(libgrpc ext_grpc)
+
+# Add libprotobuf
+add_library(libprotobuf STATIC IMPORTED)
+set_target_properties(
+  libprotobuf
+  PROPERTIES IMPORTED_LOCATION
+             ${BINARY_DIR}/third_party/protobuf/libprotobuf.a
+             INTERFACE_INCLUDE_DIRECTORIES
+             ${SOURCE_DIR}/third_party/protobuf/src)
+add_dependencies(libgrpc libprotobuf)
+
+message(STATUS "protibuf include dirs ${SOURCE_DIR}/third_party/protobuf/src")
+
+# Add protoc
+add_executable(protoc IMPORTED)
+set_target_properties(protoc
+                      PROPERTIES IMPORTED_LOCATION
+                                 ${BINARY_DIR}/third_party/protobuf/protoc)
+
+# Generate protobuf headers
+get_filename_component(hw_proto ${PROJECT_SOURCE_DIR}/protos/helloworld.proto
+                       ABSOLUTE)
+get_filename_component(hw_proto_path "${hw_proto}" PATH)
+
+set(_PROTOBUF_PROTOC ${BINARY_DIR}/third_party/protobuf/protoc)
+set(_GRPC_CPP_PLUGIN_EXECUTABLE ${BINARY_DIR}/grpc_cpp_plugin)
+
+# Generated sources
+set(hw_proto_srcs "${CMAKE_CURRENT_BINARY_DIR}/helloworld.pb.cc")
+set(hw_proto_hdrs "${CMAKE_CURRENT_BINARY_DIR}/helloworld.pb.h")
+set(hw_grpc_srcs "${CMAKE_CURRENT_BINARY_DIR}/helloworld.grpc.pb.cc")
+set(hw_grpc_hdrs "${CMAKE_CURRENT_BINARY_DIR}/helloworld.grpc.pb.h")
+
+message(STATUS "_PROTOBUF_PROTOC ${_PROTOBUF_PROTOC}")
+message(STATUS "CMAKE_CURRENT_BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}")
+message(STATUS "hw_proto_path ${hw_proto_path}")
+message(STATUS "_GRPC_CPP_PLUGIN_EXECUTABLE ${_GRPC_CPP_PLUGIN_EXECUTABLE}")
+message(STATUS "hw_proto ${hw_proto}")
+
+add_custom_target(generate_protos)
+add_custom_command(
+  TARGET generate_protos
+  # OUTPUT "${hw_proto_srcs}"
+  # "${hw_proto_hdrs}"
+  # "${hw_grpc_srcs}"
+  # "${hw_grpc_hdrs}"
+  COMMAND ${_PROTOBUF_PROTOC}
+          ARGS
+          --grpc_out
+          "${CMAKE_CURRENT_BINARY_DIR}"
+          --cpp_out
+          "${CMAKE_CURRENT_BINARY_DIR}"
+          -I
+          "${hw_proto_path}"
+          --plugin=protoc-gen-grpc="${_GRPC_CPP_PLUGIN_EXECUTABLE}"
+          "${hw_proto}"
+  DEPENDS "${hw_proto}" libprotobuf)
+
+# Include generated *.pb.h files
+include_directories("${CMAKE_CURRENT_BINARY_DIR}")
+
+add_dependencies(libgrpc generate_protos)
--- test/test_grpc.cpp
+++ test/test_grpc.cpp
@@ -15,6 +15,56 @@
 //*****************************************************************************
 
 #include <grpcpp/grpcpp.h>
+#include <iostream>
+#include <memory>
+#include <string>
+#include "helloworld.grpc.pb.h"
+
 #include "gtest/gtest.h"
 
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+using helloworld::Greeter;
+using helloworld::HelloReply;
+using helloworld::HelloRequest;
+
 TEST(grpc, trivial) { EXPECT_EQ(1, 1); }
+
+TEST(grpc, greeter_client) {
+  class GreeterClient {
+   public:
+    GreeterClient(std::shared_ptr<Channel> channel)
+        : stub_(Greeter::NewStub(channel)) {}
+
+    // Assembles the client's payload, sends it and presents the response back
+    // from the server.
+    std::string SayHello(const std::string& user) {
+      // Data we are sending to the server.
+      HelloRequest request;
+      request.set_name(user);
+
+      // Container for the data we expect from the server.
+      HelloReply reply;
+
+      // Context for the client. It could be used to convey extra information to
+      // the server and/or tweak certain RPC behaviors.
+      ClientContext context;
+
+      // The actual RPC.
+      Status status = stub_->SayHello(&context, request, &reply);
+
+      // Act upon its status.
+      if (status.ok()) {
+        return reply.message();
+      } else {
+        std::cout << status.error_code() << ": " << status.error_message()
+                  << std::endl;
+        return "RPC failed";
+      }
+    }
+
+   private:
+    std::unique_ptr<Greeter::Stub> stub_;
+  };
+}
