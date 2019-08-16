c8c73b0902643df66fc907581e8742eed561a00f
Fabian Boemer <fabian.boemer@intel.com>
2019-08-16 08:25:02 -0700

    Cleanup

--- cmake/json.cmake
+++ cmake/json.cmake
@@ -18,10 +18,6 @@ include(ExternalProject)
 
 set(JSON_PREFIX ${CMAKE_CURRENT_BINARY_DIR}/json)
 set(JSON_SRC_DIR ${JSON_PREFIX}/src/ext_json)
-
-message("JSON_PREFIX ${JSON_PREFIX}")
-message("JSON_SRC_DIR ${JSON_SRC_DIR}")
-
 set(JSON_GIT_REPO_URL https://github.com/nlohmann/json)
 set(JSON_GIT_LABEL v3.5.0)
 
@@ -29,18 +25,13 @@ ExternalProject_Add(ext_json
                     PREFIX json
                     GIT_REPOSITORY ${JSON_GIT_REPO_URL}
                     GIT_TAG ${JSON_GIT_LABEL}
-                    # Disable install step
                     CONFIGURE_COMMAND ""
                     BUILD_COMMAND cp
                                   -r
                                   ${JSON_SRC_DIR}/single_include/nlohmann
                                   ${EXTERNAL_INSTALL_DIR}/include/
                     INSTALL_COMMAND ""
-                                    # INSTALL_DIR ${EXTERNAL_INSTALL_DIR}
-                                    # UPDATE_COMMAND ""
                     EXCLUDE_FROM_ALL TRUE)
-
-# ------------------------------------------------------------------------------
 
 ExternalProject_Get_Property(ext_json SOURCE_DIR)
 add_library(libjson INTERFACE)
