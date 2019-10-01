e16e4e8a7916696b6cba010fd6f70c9167c1ed08
Fabian Boemer <fabian.boemer@intel.com>
2019-10-01 15:02:26 -0700

    Fix relu seal

--- src/seal/kernel/relu_seal.cpp
+++ src/seal/kernel/relu_seal.cpp
@@ -14,13 +14,12 @@
 // limitations under the License.
 //*****************************************************************************
 
-#pragma once
-
 #include <memory>
 #include <vector>
 
 #include "ngraph/type/element_type.hpp"
 #include "seal/he_seal_backend.hpp"
+#include "seal/kernel/relu_seal.hpp"
 #include "seal/seal_ciphertext_wrapper.hpp"
 #include "seal/seal_plaintext_wrapper.hpp"
 #include "seal/seal_util.hpp"
@@ -90,6 +89,3 @@ void ngraph::he::relu_seal(
     scalar_relu_seal(*arg[i], out[i], he_seal_backend);
   }
 }
-
-}  // namespace he
-}  // namespace ngraph
