902439eb356908ffa73667a2637ecaf926929dca
Fabian Boemer <fabian.boemer@intel.com>
2019-10-10 11:29:52 -0700

    Added relu tests

--- test/test_relu.in.cpp
+++ test/test_relu.in.cpp
@@ -14,6 +14,7 @@
 // limitations under the License.
 //*****************************************************************************
 
+#include "he_op_annotations.hpp"
 #include "ngraph/ngraph.hpp"
 #include "seal/he_seal_backend.hpp"
 #include "test_util.hpp"
@@ -24,161 +25,102 @@
 
 using namespace std;
 using namespace ngraph;
+using namespace ngraph::he;
 
 static string s_manifest = "${MANIFEST}";
 
-NGRAPH_TEST(${BACKEND_NAME}, relu_plain_2_3) {
+auto relu_test = [](const ngraph::Shape& shape, const bool arg1_encrypted,
+                    const bool complex_packing, const bool packed) {
   auto backend = runtime::Backend::create("${BACKEND_NAME}");
   auto he_backend = static_cast<ngraph::he::HESealBackend*>(backend.get());
 
-  Shape shape{2, 3};
+  if (complex_packing) {
+    he_backend->update_encryption_parameters(
+        ngraph::he::HESealEncryptionParameters::
+            default_complex_packing_parms());
+  }
+
   auto a = make_shared<op::Parameter>(element::f32, shape);
   auto t = make_shared<op::Relu>(a);
   auto f = make_shared<Function>(t, ParameterVector{a});
 
-  auto t_a = he_backend->create_plain_tensor(element::f32, shape);
-  auto t_result = he_backend->create_plain_tensor(element::f32, shape);
+  auto annotation_from_flags = [](bool is_encrypted, bool is_packed) {
+    if (is_encrypted && is_packed) {
+      return HEOpAnnotations::server_ciphertext_packed_annotation();
+    } else if (is_encrypted && !is_packed) {
+      return HEOpAnnotations::server_ciphertext_unpacked_annotation();
+    } else if (!is_encrypted && is_packed) {
+      return HEOpAnnotations::server_plaintext_packed_annotation();
+    } else if (!is_encrypted && !is_packed) {
+      return HEOpAnnotations::server_ciphertext_unpacked_annotation();
+    }
+    throw ngraph_error("Logic error");
+  };
 
-  copy_data(t_a, vector<float>{-1, -0.5, 0., 0.5, 1, 8});
+  a->set_op_annotations(annotation_from_flags(arg1_encrypted, packed));
 
-  auto handle = backend->compile(f);
-  handle->call_with_validate({t_result}, {t_a});
-  EXPECT_TRUE(all_close(read_vector<float>(t_result),
-                        vector<float>{0, 0, 0, 0.5, 1, 8}, 1e-3f));
-}
+  auto tensor_from_flags = [&](bool encrypted) {
+    if (encrypted && packed) {
+      return he_backend->create_packed_cipher_tensor(element::f32, shape);
+    } else if (encrypted && !packed) {
+      return he_backend->create_cipher_tensor(element::f32, shape);
+    } else if (!encrypted && packed) {
+      return he_backend->create_packed_plain_tensor(element::f32, shape);
+    } else if (!encrypted && !packed) {
+      return he_backend->create_plain_tensor(element::f32, shape);
+    }
+    throw ngraph_error("Logic error");
+  };
 
-NGRAPH_TEST(${BACKEND_NAME}, relu_plain_2_3_complex) {
-  auto backend = runtime::Backend::create("${BACKEND_NAME}");
-  auto he_backend = static_cast<ngraph::he::HESealBackend*>(backend.get());
-  he_backend->update_encryption_parameters(
-      ngraph::he::HESealEncryptionParameters::default_complex_packing_parms());
+  auto t_a = tensor_from_flags(arg1_encrypted);
+  auto t_result = tensor_from_flags(arg1_encrypted);
 
-  Shape shape{2, 3};
-  auto a = make_shared<op::Parameter>(element::f32, shape);
-  auto t = make_shared<op::Relu>(a);
-  auto f = make_shared<Function>(t, ParameterVector{a});
+  std::vector<float> input_a;
+  std::vector<float> exp_result;
 
-  auto t_a = he_backend->create_plain_tensor(element::f32, shape);
-  auto t_result = he_backend->create_plain_tensor(element::f32, shape);
-
-  copy_data(t_a, vector<float>{-1, -0.5, 0., 0.5, 1, 8});
-
-  auto handle = backend->compile(f);
-  handle->call_with_validate({t_result}, {t_a});
-  EXPECT_TRUE(all_close(read_vector<float>(t_result),
-                        vector<float>{0, 0, 0, 0.5, 1, 8}, 1e-3f));
-}
-
-NGRAPH_TEST(${BACKEND_NAME}, relu_cipher_2_3) {
-  auto backend = runtime::Backend::create("${BACKEND_NAME}");
-  auto he_backend = static_cast<ngraph::he::HESealBackend*>(backend.get());
-
-  Shape shape{2, 3};
-  auto a = make_shared<op::Parameter>(element::f32, shape);
-  auto t = make_shared<op::Relu>(a);
-  auto f = make_shared<Function>(t, ParameterVector{a});
-
-  auto t_a = he_backend->create_cipher_tensor(element::f32, shape);
-  auto t_result = he_backend->create_cipher_tensor(element::f32, shape);
-
-  copy_data(t_a, vector<float>{-1, -0.5, 0., 0.5, 1, 1.5});
+  for (int i = 0; i < ngraph::shape_size(shape); ++i) {
+    if (i % 2 == 0) {
+      input_a.emplace_back(i);
+    } else {
+      input_a.emplace_back(1 - i);
+    }
+    exp_result.emplace_back(input_a.back() > 0 ? input_a.back() : 0);
+  }
+  copy_data(t_a, input_a);
 
   auto handle = backend->compile(f);
   handle->call_with_validate({t_result}, {t_a});
-  EXPECT_TRUE(all_close(read_vector<float>(t_result),
-                        vector<float>{0, 0, 0, 0.5, 1, 1.5}, 1e-3f));
+  EXPECT_TRUE(all_close(read_vector<float>(t_result), exp_result, 1e-3f));
+};
+
+NGRAPH_TEST(${BACKEND_NAME}, relu_2_3_plain_real_unpacked) {
+  relu_test(ngraph::Shape{2, 3}, false, false, false);
 }
 
-NGRAPH_TEST(${BACKEND_NAME}, relu_cipher_2_3_complex) {
-  auto backend = runtime::Backend::create("${BACKEND_NAME}");
-  auto he_backend = static_cast<ngraph::he::HESealBackend*>(backend.get());
-  he_backend->update_encryption_parameters(
-      ngraph::he::HESealEncryptionParameters::default_complex_packing_parms());
-
-  Shape shape{2, 3};
-  auto a = make_shared<op::Parameter>(element::f32, shape);
-  auto t = make_shared<op::Relu>(a);
-  auto f = make_shared<Function>(t, ParameterVector{a});
-
-  auto t_a = he_backend->create_cipher_tensor(element::f32, shape);
-  auto t_result = he_backend->create_cipher_tensor(element::f32, shape);
-
-  copy_data(t_a, vector<float>{-1, -0.5, 0., 0.5, 1, 1.5});
-
-  auto handle = backend->compile(f);
-  handle->call_with_validate({t_result}, {t_a});
-  EXPECT_TRUE(all_close(read_vector<float>(t_result),
-                        vector<float>{0, 0, 0, 0.5, 1, 1.5}, 1e-3f));
+NGRAPH_TEST(${BACKEND_NAME}, relu_2_3_plain_real_packed) {
+  relu_test(ngraph::Shape{2, 3}, false, false, true);
 }
 
-NGRAPH_TEST(${BACKEND_NAME}, relu_packed_plain_2_3) {
-  auto backend = runtime::Backend::create("${BACKEND_NAME}");
-  auto he_backend = static_cast<ngraph::he::HESealBackend*>(backend.get());
-
-  Shape shape{2, 3};
-  auto a = make_shared<op::Parameter>(element::f32, shape);
-  auto t = make_shared<op::Relu>(a);
-  auto f = make_shared<Function>(t, ParameterVector{a});
-
-  auto t_a = he_backend->create_packed_plain_tensor(element::f32, shape);
-  auto t_result = he_backend->create_packed_plain_tensor(element::f32, shape);
-
-  copy_data(t_a, vector<float>{-1, -0.5, 0., 0.5, 1, 8});
-
-  auto handle = backend->compile(f);
-  handle->call_with_validate({t_result}, {t_a});
-  EXPECT_TRUE(all_close(read_vector<float>(t_result),
-                        vector<float>{0, 0, 0, 0.5, 1, 8}, 1e-3f));
+NGRAPH_TEST(${BACKEND_NAME}, relu_2_3_plain_complex_unpacked) {
+  relu_test(ngraph::Shape{2, 3}, false, true, false);
 }
 
-NGRAPH_TEST(${BACKEND_NAME}, relu_packed_cipher_2_3) {
-  auto backend = runtime::Backend::create("${BACKEND_NAME}");
-  auto he_backend = static_cast<ngraph::he::HESealBackend*>(backend.get());
-
-  Shape shape{2, 3};
-  auto a = make_shared<op::Parameter>(element::f32, shape);
-  auto t = make_shared<op::Relu>(a);
-  auto f = make_shared<Function>(t, ParameterVector{a});
-
-  auto t_a = he_backend->create_packed_cipher_tensor(element::f32, shape);
-  auto t_result = he_backend->create_packed_cipher_tensor(element::f32, shape);
-
-  copy_data(t_a, vector<float>{-1, -0.5, 0., 0.5, 1, 1.5});
-
-  auto handle = backend->compile(f);
-  handle->call_with_validate({t_result}, {t_a});
-  EXPECT_TRUE(all_close(read_vector<float>(t_result),
-                        vector<float>{0, 0, 0, 0.5, 1, 1.5}, 1e-3f));
+NGRAPH_TEST(${BACKEND_NAME}, relu_2_3_plain_complex_packed) {
+  relu_test(ngraph::Shape{2, 3}, false, true, true);
 }
 
-NGRAPH_TEST(${BACKEND_NAME}, pad_relu) {
-  auto backend = runtime::Backend::create("${BACKEND_NAME}");
-  auto he_backend = static_cast<ngraph::he::HESealBackend*>(backend.get());
+NGRAPH_TEST(${BACKEND_NAME}, relu_2_3_cipher_real_unpacked) {
+  relu_test(ngraph::Shape{2, 3}, true, false, false);
+}
 
-  size_t batch_size = 1;
+NGRAPH_TEST(${BACKEND_NAME}, relu_2_3_cipher_real_packed) {
+  relu_test(ngraph::Shape{2, 3}, true, false, true);
+}
 
-  Shape shape{batch_size, 3};
-  Shape result_shape{batch_size, 5};
-  auto a = make_shared<op::Parameter>(element::f32, shape);
-  auto b = op::Constant::create(element::f32, Shape{}, vector<float>({0}));
-  CoordinateDiff padding_below{0, 1};
-  CoordinateDiff padding_above{0, 1};
-  auto c = make_shared<op::Pad>(a, b, padding_below, padding_above);
-  auto relu = make_shared<op::Relu>(c);
-  auto f = make_shared<Function>(relu, ParameterVector{a});
+NGRAPH_TEST(${BACKEND_NAME}, relu_2_3_cipher_complex_unpacked) {
+  relu_test(ngraph::Shape{2, 3}, true, true, false);
+}
 
-  // Server inputs which are not used
-  auto t_dummy = he_backend->create_cipher_tensor(element::f32, shape);
-  auto t_result = he_backend->create_cipher_tensor(element::f32, result_shape);
-
-  // Used for dummy server inputs
-  float DUMMY_FLOAT = 99;
-  copy_data(t_dummy, vector<float>{DUMMY_FLOAT, DUMMY_FLOAT, DUMMY_FLOAT});
-
-  auto handle = he_backend->compile(f);
-  handle->call_with_validate({t_result}, {t_dummy});
-
-  EXPECT_TRUE(all_close(
-      read_vector<float>(t_result),
-      vector<float>{0, DUMMY_FLOAT, DUMMY_FLOAT, DUMMY_FLOAT, 0}, 1e-3f));
+NGRAPH_TEST(${BACKEND_NAME}, relu_2_3_cipher_complex_packed) {
+  relu_test(ngraph::Shape{2, 3}, true, true, true);
 }
