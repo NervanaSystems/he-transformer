a8d8af22f8d36993fee0d4515b99c0f6219d5b28
Fabian Boemer <fabian.boemer@intel.com>
2019-10-30 13:02:41 -0700

    Faster relu on small batch sizes

--- src/seal/kernel/relu_seal.cpp
+++ src/seal/kernel/relu_seal.cpp
@@ -44,6 +44,7 @@ void scalar_relu_seal(const HEType& arg, HEType& out,
     HEPlaintext plain;
     decrypt(plain, *arg.get_ciphertext(), arg.complex_packing(), decryptor,
             ckks_encoder);
+    plain.resize(arg.batch_size());
     scalar_relu_seal(plain, plain);
     encrypt(out.get_ciphertext(), plain, parms_id, ngraph::element::f32, scale,
             ckks_encoder, encryptor, arg.complex_packing());
