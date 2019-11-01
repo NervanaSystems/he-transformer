529e92c7dc4ceb9306a04d132d28eeded5b54f03
Fabian Boemer <fabian.boemer@intel.com>
2019-11-01 15:40:39 -0700

    Fix include directories

--- cmake/ngraph-tf.cmake
+++ cmake/ngraph-tf.cmake
@@ -83,8 +83,8 @@ add_library(ngraph SHARED IMPORTED)
 set_target_properties(ngraph
                       PROPERTIES IMPORTED_LOCATION
                                  ${NGRAPH_TF_LIB_DIR}/libngraph.so)
-set_target_properties(ngraph
-                      PROPERTIES INCLUDE_DIRECTORIES ${NGRAPH_TF_INCLUDE_DIR})
+
+target_include_directories(ngraph SYSTEM INTERFACE "${NGRAPH_TF_INCLUDE_DIR}")
 
 install(DIRECTORY ${NGRAPH_TF_LIB_DIR}/
         DESTINATION ${EXTERNAL_INSTALL_LIB_DIR}
